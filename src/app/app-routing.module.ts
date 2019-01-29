56f1bf698da3c8bb605f134cc9bf4d106daa790d
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2019-01-29 09:14:24 +0700

    fix home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './container/home-page/home-page.component';
 
 const routes: Routes = [
+  {path: '', component: HomePageComponent},
   {path: 'movie', loadChildren: './movies/movies.module#MoviesModule'},
   {path: 'tv', loadChildren: './tv/tv.module#TvModule'},
   {path: 'person', loadChildren: './person/person.module#PersonModule'},
